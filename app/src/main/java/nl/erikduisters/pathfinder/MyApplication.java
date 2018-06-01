d95167affc34f78bbaeb77b136a3e3056fcc3b5b
Erik Duisters <e.duisters@outlook.com>
2018-06-01 17:58:31 +0200

    Added Firebase Crashlytics Disabled automatic Crashlytics initialization Manually initialise Crashlytics in MyApplication.onCreate()

--- app/src/main/java/nl/erikduisters/pathfinder/MyApplication.java
+++ app/src/main/java/nl/erikduisters/pathfinder/MyApplication.java
@@ -3,14 +3,17 @@ package nl.erikduisters.pathfinder;
 import android.app.Application;
 import android.support.annotation.NonNull;
 
+import com.crashlytics.android.Crashlytics;
 import com.squareup.leakcanary.LeakCanary;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
  * Created by Erik Duisters on 01-06-2018.
  */
 
+//TODO: Before submission create a readme file that explains that the reviewer needs to provide his/her own google-services.json file downloadable from the firebase console
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
@@ -27,6 +30,9 @@ public class MyApplication extends Application {
         } else {
             Timber.plant(new ReleaseTree());
         }
+
+        //TODO: Maybe make this optional through a preference
+        Fabric.with(this, new Crashlytics());
     }
 
     private static class ReleaseTree extends Timber.Tree {
